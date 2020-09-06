361af7edaaa714719655c2a65b4bfed56439dc53
Jeremy Langevin <langevinj832@gmail.com>
2020-09-06 15:45:50 -0400

    further work on the relationship and backref

--- models.py
+++ models.py
@@ -1,12 +1,14 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, date, tzinfo, timezone, timedelta
+from datetime import datetime
 
 db = SQLAlchemy()
+
 
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -14,22 +16,23 @@ class User(db.Model):
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
-    
+
     first_name = db.Column(db.Text,
                            nullable=False)
-    
+
     last_name = db.Column(db.Text,
                           nullable=False)
 
     image_url = db.Column(db.String,
                           nullable=True,
                           default="https://i.stack.imgur.com/dr5qp.jpg")
-    
-    post = db.relationship('Post', cascade='all, delete')
+
+    posts = db.relationship('Post', backref="user", cascade="all, delete-orphan")
 
     def __repr__(self):
         u = self
         return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}>"
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -43,27 +46,11 @@ class Post(db.Model):
 
     content = db.Column(db.Text,
                         nullable=False)
-    
+
     created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
-    
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), db.relationship("User", cascade="all, delete-orphan"))
 
-    user_id = db.Column(db.ForeignKey('users.id'))
-
-    user = db.relationship('User')
-
-
-    
-    # def __init__(self, title, content):
-    #     self.id = self.id
-    #     self.title = title
-    #     self.content = content
-    #     self.created_at = datetime.now()
-
-    # datetime.datetime(2020, 9, 6, 8, 12, 52, 396813)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
         p = self
         return f"<Post id={p.id} title={p.title} created_at={p.created_at} user={p.user_id}"
-   
-    
--- test_flask.py
+++ test_flask.py
@@ -2,98 +2,26 @@ from unittest import TestCase
 
 from app import app
 from models import db, User, Post
-#Use a test database
+
+
+# Use a test database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_db_test'
 app.config['SQLALCHEMY_ECHO'] = False
 
-#Make Flask errors be real errors
+# Make Flask errors be real errors
 app.config['TESTING'] = True
 
-#Don't use Flask DebugToolbar
+# Don't use Flask DebugToolbar
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 db.drop_all()
 db.create_all()
 
-# class UserTestCase(TestCase):
-#     """Tests for views of Users"""
-
-#     def setUp(self):
-#         """Add sample user"""
-
-#         User.query.delete()
-
-#         user = User(first_name="Jim", last_name="Bob", image_url="https://npr.brightspotcdn.com/dims4/default/83db39c/2147483647/strip/true/crop/640x427+0+0/resize/1760x1174!/format/webp/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fksor%2Ffiles%2F201711%2Fdennis_richardson.jpg")
-#         db.session.add(user)
-#         db.session.commit()
-
-#         self.user_id = user.id
-#         self.user = user
-
-#     def tearDown(self):
-#         """Clean up"""
-
-#         db.session.rollback()
-
-#     def test_list_users(self):
-#         with app.test_client() as client:
-#             resp = client.get("/", follow_redirects=True)
-#             self.assertEqual(resp.status_code, 200)
-
-#     def test_linked_users(self):
-#         with app.test_client() as client:
-#             resp = client.get("/users")
-#             html = resp.get_data(as_text=True)
-
-#             self.assertEqual(resp.status_code, 200)
-#             self.assertIn('Jim Bob', html)
-    
-#     def test_add_user_form(self):
-#         with app.test_client() as client:
-#             resp = client.get("/users/new")
-
-#             self.assertEqual(resp.status_code, 200)
-    
-#     def test_add_user(self):
-#         with app.test_client() as client:
-#             d = {"first_name": "John", "last_name": "Tom", "image_url": "www.wikipedia.com.jpg"}
-#             resp = client.post("/users/new", data=d, follow_redirects=True)
-#             html = resp.get_data(as_text=True)
-
-#             self.assertEqual(resp.status_code, 200)
-#             self.assertIn('<li><a href="/users/2">John Tom</a></li>', html)
-    
-#     def test_details_page(self):
-#         with app.test_client() as client:
-#             resp = client.get(f'/users/{self.user.id}')
-#             html = resp.get_data(as_text=True)
-
-#             self.assertEqual(resp.status_code, 200)
-#             self.assertIn("<h2>Jim Bob</h2>", html)
-    
-#     def test_edit_user(self):
-#         with app.test_client() as client:
-#             resp = client.get(f'/users/{self.user.id}/edit')
-#             html = resp.get_data(as_text=True)
-
-#             self.assertEqual(resp.status_code, 200)
-#             self.assertIn('<h2>Edit a User</h2>', html)
-    
-#     def test_edit_db_user(self):
-#         with app.test_client() as client:
-#             d = {"first_name": "John", "last_name": "Tom", "image_url": "www.wikipedia.com.jpg"}
-#             resp = client.post(f'/users/{self.user.id}/edit', data=d, follow_redirects=True)
-#             html = resp.get_data(as_text=True)
-
-#             self.assertEqual(resp.status_code, 200)
-#             self.assertIn("John Tom", html)
-#             self.assertNotIn("Jim Bob", html)
-
-class PostTestCase(TestCase):
-    """Tests for vies for Posts"""
+class UserTestCase(TestCase):
+    """Tests for views of Users"""
 
     def setUp(self):
-        """Add sample user and post"""
+        """Add sample user"""
 
         User.query.delete()
 
@@ -104,19 +32,97 @@ class PostTestCase(TestCase):
         self.user_id = user.id
         self.user = user
 
-        Post.query.delete()
+    def tearDown(self):
+        """Clean up"""
 
-        new_post = Post(title="Garage Sale", content="Huge multi-family sale this weekend", user_id=1)
-        db.session.add(new_post)
+        db.session.rollback()
+
+    def test_list_users(self):
+        with app.test_client() as client:
+            resp = client.get("/", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+    def test_linked_users(self):
+        with app.test_client() as client:
+            resp = client.get("/users")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Jim Bob', html)
+
+    def test_add_user_form(self):
+        with app.test_client() as client:
+            resp = client.get("/users/new")
+
+            self.assertEqual(resp.status_code, 200)
+
+    def test_add_user(self):
+        with app.test_client() as client:
+            d = {"first_name": "John", "last_name": "Tom",
+                 "image_url": "www.wikipedia.com.jpg"}
+            resp = client.post("/users/new", data=d, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<li><a href="/users/2">John Tom</a></li>', html)
+
+    def test_details_page(self):
+        with app.test_client() as client:
+            resp = client.get(f'/users/{self.user.id}')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("<h2>Jim Bob</h2>", html)
+
+    def test_edit_user(self):
+        with app.test_client() as client:
+            resp = client.get(f'/users/{self.user.id}/edit')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h2>Edit a User</h2>', html)
+
+    def test_edit_db_user(self):
+        with app.test_client() as client:
+            d = {"first_name": "John", "last_name": "Tom",
+                 "image_url": "www.wikipedia.com.jpg"}
+            resp = client.post(
+                f'/users/{self.user.id}/edit', data=d, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("John Tom", html)
+            self.assertNotIn("Jim Bob", html)
+
+
+class PostTestCase(TestCase):
+    """Tests for views for Posts"""
+
+    def setUp(self):
+        """Add sample user and post"""
+        
+        User.query.delete()
+        user = User(first_name="Jim", last_name="Bob", image_url="https://npr.brightspotcdn.com/dims4/default/83db39c/2147483647/strip/true/crop/640x427+0+0/resize/1760x1174!/format/webp/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fksor%2Ffiles%2F201711%2Fdennis_richardson.jpg")
+        db.session.add(user)
         db.session.commit()
 
-        self.post_id = post.id 
+        self.user_id = user.id
+        self.user = user
+
+        Post.query.delete()
+        
+        new_post = Post(title="Garage Sale",
+                        content="Huge multi-family sale this weekend", user_id=1)
+
+        db.session.add(new_post)
+        db.session.commit()
+        self.post_id = new_post.id
         self.post = new_post
 
     def tearDown(self):
         """Clean up"""
         db.session.rollback()
-    
+
     def test_add_post_page(self):
         with app.test_client() as client:
             resp = client.get(f"/users/{self.post.user_id}/posts/new")
@@ -125,21 +131,24 @@ class PostTestCase(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn('<title>Add a New Post</title>', html)
             self.assertIn('<h2>Add Post for Jim Bob</h2>', html)
-    
-    #seem to be getting an error with this test, but in practice this works
-    # def test_add_post(self):
-    #     with app.test_client() as client:
-    #         p = {"title": "Wow i feel sick", "content":"I really need a checkup"}
-    #         resp = client.post(f"/users/{self.user_id}/posts/new", data=p, follow_redirects=True)
-    #         html = resp.get_data(as_text=True)
 
-    #         self.assertEqual(resp.status_code, 200)
-            # self.assertIn('<li><a href="/posts/2">Wow i feel sick</a></li>', html)
+    # seem to be getting an error with this test, but in practice this works
 
-    def test_show_post(self):
+    def test_add_post(self):
         with app.test_client() as client:
-            resp = client.get('/posts/1')
-            html = resp.get_date(as_text=True)
+            p = {"title": "Wow i feel sick",
+                 "content": "I really need a checkup"}
+            resp = client.post(
+                f"/users/{self.user_id}/posts/new", data=p, follow_redirects=True)
+            html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("<title>Garage Sale</title>", html)
+            self.assertIn('<li><a href="/posts/2">Wow i feel sick</a></li>', html)
+
+    # def test_show_post(self):
+    #     with app.test_client() as client:
+    #         resp = client.get('/posts/1')
+    #         html = resp.get_date(as_text=True)
+
+    #         self.assertEqual(resp.status_code, 200)
+    #         self.assertIn("<title>Garage Sale</title>", html)
