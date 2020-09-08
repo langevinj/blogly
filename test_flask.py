f94afee5263caa2a067972be05b10e1b71d6b82c
Jeremy Langevin <langevinj832@gmail.com>
2020-09-08 09:23:07 -0400

    post tests added

--- test_flask.py
+++ test_flask.py
@@ -22,15 +22,23 @@ class UserTestCase(TestCase):
 
     def setUp(self):
         """Add sample user"""
-
+        Post.query.delete()
         User.query.delete()
+        
 
         user = User(first_name="Jim", last_name="Bob", image_url="https://npr.brightspotcdn.com/dims4/default/83db39c/2147483647/strip/true/crop/640x427+0+0/resize/1760x1174!/format/webp/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fksor%2Ffiles%2F201711%2Fdennis_richardson.jpg")
         db.session.add(user)
+        db.session.flush()
+
+        new_post = Post(title="Garage Sale",
+                        content="Huge multi-family sale this weekend", user_id=user.id)
+        db.session.add(new_post)
         db.session.commit()
 
         self.user_id = user.id
         self.user = user
+        self.post_id = new_post.id
+        self.post = new_post
 
     def tearDown(self):
         """Clean up"""
@@ -63,8 +71,10 @@ class UserTestCase(TestCase):
             resp = client.post("/users/new", data=d, follow_redirects=True)
             html = resp.get_data(as_text=True)
 
+            user=User.query.filter_by(last_name="Tom").first()
+
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<li><a href="/users/2">John Tom</a></li>', html)
+            self.assertIn(user.first_name, 'John')
 
     def test_details_page(self):
         with app.test_client() as client:
@@ -93,62 +103,24 @@ class UserTestCase(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn("John Tom", html)
             self.assertNotIn("Jim Bob", html)
-
-
-class PostTestCase(TestCase):
-    """Tests for views for Posts"""
-
-    def setUp(self):
-        """Add sample user and post"""
-        
-        User.query.delete()
-        user = User(first_name="Jim", last_name="Bob", image_url="https://npr.brightspotcdn.com/dims4/default/83db39c/2147483647/strip/true/crop/640x427+0+0/resize/1760x1174!/format/webp/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fksor%2Ffiles%2F201711%2Fdennis_richardson.jpg")
-        db.session.add(user)
-        db.session.commit()
-
-        self.user_id = user.id
-        self.user = user
-
-        Post.query.delete()
-        
-        new_post = Post(title="Garage Sale",
-                        content="Huge multi-family sale this weekend", user_id=1)
-
-        db.session.add(new_post)
-        db.session.commit()
-        self.post_id = new_post.id
-        self.post = new_post
-
-    def tearDown(self):
-        """Clean up"""
-        db.session.rollback()
-
+    
     def test_add_post_page(self):
         with app.test_client() as client:
             resp = client.get(f"/users/{self.post.user_id}/posts/new")
             html = resp.get_data(as_text=True)
+            user = User.query.get(self.user_id)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<title>Add a New Post</title>', html)
-            self.assertIn('<h2>Add Post for Jim Bob</h2>', html)
+            self.assertIn(f'<h2>Add Post for {user.first_name} {user.last_name}</h2>', html)
 
-    # seem to be getting an error with this test, but in practice this works
+    def test_add_post(self):
+        with app.test_client() as client:
+            p = {"title": "Wow i feel sick",
+                 "content": "I really need a checkup"}
+            resp = client.post(
+                f"/users/{self.user_id}/posts/new", data=p, follow_redirects=True)
+            html = resp.get_data(as_text=True)
 
-    # def test_add_post(self):
-    #     with app.test_client() as client:
-    #         p = {"title": "Wow i feel sick",
-    #              "content": "I really need a checkup"}
-    #         resp = client.post(
-    #             f"/users/{self.user_id}/posts/new", data=p, follow_redirects=True)
-    #         html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<li><a href="/posts/2">Wow i feel sick</a></li>', html)
 
-    #         self.assertEqual(resp.status_code, 200)
-    #         self.assertIn('<li><a href="/posts/2">Wow i feel sick</a></li>', html)
-
-    # def test_show_post(self):
-    #     with app.test_client() as client:
-    #         resp = client.get('/posts/1')
-    #         html = resp.get_date(as_text=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         self.assertIn("<title>Garage Sale</title>", html)
